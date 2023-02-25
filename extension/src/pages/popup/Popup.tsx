7e481036a1b3ccf186132f4d3a41f0e28c0a3c8b
Elwan Mayencourt <elwan.mayencourt@gmail.com>
2023-02-25 14:58:08 +0100

    💄 File dropzone

--- extension/src/pages/popup/Popup.tsx
+++ extension/src/pages/popup/Popup.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import logo from "@assets/img/logo.svg";
+import { useDropzone } from "react-dropzone";
 
 export default function Popup(): JSX.Element {
-    // send a message to the content script
+    const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        if (!acceptedFiles) return;
+        setSelectedFiles(acceptedFiles);
+    }, []);
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+		accept: {
+			"application/pdf": [".pdf", ".PDF"],
+		}
+    });
 
     const handleClick = () => {
         chrome.tabs.query(
@@ -52,7 +65,51 @@ export default function Popup(): JSX.Element {
     };
 
     return (
-        <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800 text-white">
+        <div className="text-center h-full p-3 bg-gray-800 text-white">
+            <div className="flex flex-col gap-4 items-center justify-center w-full">
+                <div
+                    className="flex flex-col items-center justify-center w-full border-2 border-white bg-white bg-opacity-0 border-dashed rounded-xl cursor-pointer hover:bg-opacity-20 transition-all"
+                    {...getRootProps()}
+                >
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg
+                            aria-hidden="true"
+                            className="w-10 h-10 mb-3 "
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                            ></path>
+                        </svg>
+                        <p className="mb-2 text-sm">
+                            <span className="font-semibold">
+                                Click to upload
+                            </span>{" "}
+                            or drag and drop
+                        </p>
+                        <p className="text-xs text-gray-500">(PDF)</p>
+                    </div>
+                    <input
+                        type="file"
+                        className="hidden"
+                        {...getInputProps()}
+                    />
+                </div>
+                <div className="flex flex-wrap gap-2">
+                    {selectedFiles.map((file) => (
+                        <span className="flex items-center justify-center bg-gray-700 rounded-md px-2">
+                            {file.name}
+                        </span>
+                    ))}
+                </div>
+            </div>
+
             <button onClick={handleClick}>Send resume</button>
         </div>
     );
