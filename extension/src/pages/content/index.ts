6d588dbaee177ae9512f257ba6ced7438dd10733
Elwan Mayencourt <elwan.mayencourt@gmail.com>
2023-02-25 14:04:54 +0100

    ✨ Send resume from Popup to Content

--- extension/src/pages/content/index.ts
+++ extension/src/pages/content/index.ts
@@ -1,5 +1,16 @@
 try {
-  console.log('content script loaded');
+	console.log('content script loaded');
+	chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+		// get the textarea element
+		const textarea = document.querySelector('textarea');
+
+		// set the value of the textarea
+		textarea.value = message.resume;
+
+		// fit the textarea to the content
+		textarea.style.height = 'auto';
+		textarea.style.height = textarea.scrollHeight + 'px';
+	});
 } catch (e) {
-  console.error(e);
+	console.error(e);
 }
--- extension/src/pages/popup/Popup.tsx
+++ extension/src/pages/popup/Popup.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import React from "react";
+import logo from "@assets/img/logo.svg";
 
 export default function Popup(): JSX.Element {
-  return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
-      </header>
-    </div>
-  );
+    // send a message to the content script
+
+    const handleClick = () => {
+        chrome.tabs.query(
+            { active: true, currentWindow: true },
+            function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    resume: `John Smith
+				123 Main Street, Anytown USA 12345
+				(555) 555-1234
+				john.smith@email.com
+				
+				Objective:
+				To obtain a challenging position in a dynamic organization that will allow me to utilize my skills and experience in a meaningful way.
+				
+				Experience:
+				Acme Inc., Anytown USA
+				Senior Sales Manager, 2018 - present
+				
+				Manage a team of 10 sales representatives
+				Develop and implement sales strategies to achieve company targets
+				Conduct market research and analyze data to identify new opportunities
+				Collaborate with marketing team to develop promotional materials and campaigns
+				Maintain relationships with key clients and negotiate contracts
+				XYZ Corporation, Anytown USA
+				Sales Representative, 2014 - 2018
+				
+				Prospected and cold-called potential clients to generate new business
+				Managed sales pipeline and maintained customer relationships
+				Prepared and delivered presentations to clients and stakeholders
+				Collaborated with marketing team to develop sales materials and campaigns
+				Education:
+				Bachelor of Business Administration, Anytown University, 2014
+				
+				Major in Marketing
+				Dean's List for academic achievement
+				Skills:
+				
+				Sales management
+				Customer relationship management
+				Market research and analysis
+				Negotiation and contract management
+				Presentation skills
+				Team leadership`,
+                });
+            }
+        );
+    };
+
+    return (
+        <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800 text-white">
+            <button onClick={handleClick}>Send resume</button>
+        </div>
+    );
 }
